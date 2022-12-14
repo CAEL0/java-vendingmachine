e785d1b69a558e532ce090c1470b7ca3fefa1ed7
CAEL0 <kjongmin26@gmail.com>
2022-12-14 19:32:22 +0900

    feat(ViewConstants): View에서 사용할 상수 추가

---
+++ src/main/java/vendingmachine/view/ViewConstants.java
@@ -0,0 +1,12 @@
+package vendingmachine.view;
+
+public class ViewConstants {
+    public static final String ASKING_HOLDING_SUM = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    public static final String COIN_QUANTITY_LIST = "자판기가 보유한 동전";
+    public static final String COIN_QUANTITY = "%d원 - %d개";
+    public static final String ASKING_PRODUCT_LIST = "상품명과 가격, 수량을 입력해 주세요.";
+    public static final String ASKING_INSERTING_MONEY = "투입 금액을 입력해 주세요.";
+    public static final String INSERTING_MONEY = "투입 금액: %d원";
+    public static final String ASKING_PRODUCT_NAME = "구매할 상품명을 입력해 주세요.";
+    public static final String CHANGE = "잔돈";
+}
