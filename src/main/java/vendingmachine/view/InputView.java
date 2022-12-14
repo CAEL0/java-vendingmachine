805d20165ec61b95b28a9f8566599404a6e2e6a2
CAEL0 <kjongmin26@gmail.com>
2022-12-14 20:59:57 +0900

    feat(InputView): 자판기 보유 금액을 입력 받는 메서드 추가

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -1,8 +1,19 @@
 package vendingmachine.view;
 
+import camp.nextstep.edu.missionutils.Console;
 import vendingmachine.constant.ExceptionConstants;
 
 public class InputView {
+
+    public int readHoldingSum() {
+        System.out.println(ViewConstants.ASKING_HOLDING_SUM);
+        String input = Console.readLine();
+        System.out.println();
+
+        int parsedInput = parse(input);
+        isNonnegative(parsedInput);
+        return parsedInput;
+    }
 
     private int parse(String input) {
         try {
