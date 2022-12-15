82690ea7eaf0a452424885793154ef39e020aaa6
CAEL0 <kjongmin26@gmail.com>
2022-12-15 20:30:03 +0900

    feat(VendingMachineConstants): 자판기에서 사용되는 상수 추가

---
+++ src/main/java/vendingmachine/constant/VendingMachineConstants.java
@@ -0,0 +1,8 @@
+package vendingmachine.constant;
+
+public class VendingMachineConstants {
+    public static final String FRONT = "[";
+    public static final String BACK = "]";
+    public static final String PRODUCT_DELIMITER = ";";
+    public static final String DELIMITER = ",";
+}
