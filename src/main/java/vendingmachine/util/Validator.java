eb0cbc3c618632b5e0c73a81c426263b80938720
CAEL0 <kjongmin26@gmail.com>
2022-12-15 21:26:15 +0900

    refactor(Validator): nonnegative인지 검증하는 메서드 제거

--- src/main/java/vendingmachine/util/Validator.java
+++ src/main/java/vendingmachine/util/Validator.java
@@ -4,12 +4,6 @@ import vendingmachine.constant.ExceptionConstants;
 
 public class Validator {
 
-    public static void validateNumberIsNonnegative(int number) {
-        if (number < 0) {
-            throw new IllegalArgumentException(ExceptionConstants.NOT_NEGATIVE.getMessage());
-        }
-    }
-
     public static void validateNumberIsMultipleOfTen(int number) {
         if (number % 10 != 0) {
             throw new IllegalArgumentException(ExceptionConstants.NOT_MULTIPLE_OF_TEN.getMessage());
