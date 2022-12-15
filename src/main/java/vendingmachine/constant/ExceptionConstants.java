d0cbceda406ad241ebd82f35d77d3570b4a7e4ef
CAEL0 <kjongmin26@gmail.com>
2022-12-15 21:21:25 +0900

    feat(ExceptionConstants): 상품 가격의 최소 금액 관련 에러 메시지 추가

--- src/main/java/vendingmachine/constant/ExceptionConstants.java
+++ src/main/java/vendingmachine/constant/ExceptionConstants.java
@@ -4,7 +4,8 @@ public enum ExceptionConstants {
     INCORRECT_RANGE("입력은 Integer 범위의 정수여야 합니다."),
     NOT_NEGATIVE("입력은 음수가 아니어야 합니다."),
     NOT_MULTIPLE_OF_TEN("입력은 10의 배수여야 합니다."),
-    INCORRECT_PRODUCT_LIST("상품명, 가격, 수량은 쉼표로, 개별 상품은 대괄호로 묶어 세미콜론으로 구분해야 합니다.");
+    INCORRECT_PRODUCT_LIST("상품명, 가격, 수량은 쉼표로, 개별 상품은 대괄호로 묶어 세미콜론으로 구분해야 합니다."),
+    INCORRECT_PRICE_RANGE(String.format("상품 가격은 %d원부터 시작해야 합니다.", VendingMachineConstants.LOWEST_PRICE));
 
     private final String message;
 
