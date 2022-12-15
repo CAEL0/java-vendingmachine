271ac5d20110ff4487094cca9e00b726a7d5095e
CAEL0 <kjongmin26@gmail.com>
2022-12-15 20:42:59 +0900

    refactor(VendingMachineConstants): 정규 표현식으로 변경

--- src/main/java/vendingmachine/constant/VendingMachineConstants.java
+++ src/main/java/vendingmachine/constant/VendingMachineConstants.java
@@ -1,8 +1,8 @@
 package vendingmachine.constant;
 
 public class VendingMachineConstants {
-    public static final String FRONT = "[";
-    public static final String BACK = "]";
     public static final String PRODUCT_DELIMITER = ";";
     public static final String DELIMITER = ",";
+
+    public static final String PRODUCT_REGEX = "\\[\\w+,\\w+,\\w+\\]";
 }
