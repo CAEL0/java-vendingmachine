10de8de59e085fb3bd2dda5e6312c257598b7e42
CAEL0 <kjongmin26@gmail.com>
2022-12-15 19:48:31 +0900

    feat(VendingMachine): 보유 금액만큼 동전을 무작위로 생성

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -1,9 +1,12 @@
 package vendingmachine.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import vendingmachine.constant.ExceptionConstants;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class VendingMachine {
     private final List<Integer> coinQuantities;
@@ -23,6 +26,22 @@ public class VendingMachine {
     private void validateNumberIsMultipleOfTen(int number) {
         if (number % 10 != 0) {
             throw new IllegalArgumentException(ExceptionConstants.NOT_MULTIPLE_OF_TEN.getMessage());
+        }
+    }
+
+    private void generateCoins(int holdingSum) {
+        List<Integer> coinValues = Arrays.stream(Coin.values())
+                .map(Coin::getAmount)
+                .collect(Collectors.toList());
+
+        while (holdingSum > 0) {
+            int coin = Randoms.pickNumberInList(List.of(500, 100, 50, 10));
+            if (holdingSum >= coin) {
+                holdingSum -= coin;
+                int index = coinValues.indexOf(coin);
+                int quantity = coinQuantities.get(index);
+                coinQuantities.set(index, quantity + 1);
+            }
         }
     }
 }
