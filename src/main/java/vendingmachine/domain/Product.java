074596c68b9197db4dbaf1a6cf90e968c3898135
CAEL0 <kjongmin26@gmail.com>
2022-12-15 21:29:12 +0900

    feat(Product): 상품 가격, 상품 수량 검증

--- src/main/java/vendingmachine/domain/Product.java
+++ src/main/java/vendingmachine/domain/Product.java
@@ -1,14 +1,31 @@
 package vendingmachine.domain;
 
+import vendingmachine.constant.ExceptionConstants;
+import vendingmachine.constant.VendingMachineConstants;
+import vendingmachine.util.Validator;
+
 public class Product {
     private final String name;
     private final int price;
     private final int amount;
 
-    public Product(String name, int price, int amount) {
+    public Product(String name, String price, String amount) {
         this.name = name;
-        this.price = price;
-        this.amount = amount;
+
+        this.price = parse(price);
+        Validator.validateLowerBound(this.price, VendingMachineConstants.LOWEST_PRICE);
+        Validator.validateNumberIsMultipleOfTen(this.price);
+
+        this.amount = parse(amount);
+        Validator.validateLowerBound(this.amount, 0);
+    }
+
+    private int parse(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ExceptionConstants.INCORRECT_RANGE.getMessage());
+        }
     }
 
     public String getName() {
