8a7562ec7650487c0f1c0002d130f124c7f4f806
CAEL0 <kjongmin26@gmail.com>
2022-12-15 21:40:18 +0900

    feat(ExceptionConstants): 중복 상품명 관련 에러 메시지 추가

--- src/main/java/vendingmachine/constant/ExceptionConstants.java
+++ src/main/java/vendingmachine/constant/ExceptionConstants.java
@@ -5,7 +5,8 @@ public enum ExceptionConstants {
     NOT_NEGATIVE("입력은 음수가 아니어야 합니다."),
     NOT_MULTIPLE_OF_TEN("입력은 10의 배수여야 합니다."),
     INCORRECT_PRODUCT_LIST("상품명, 가격, 수량은 쉼표로, 개별 상품은 대괄호로 묶어 세미콜론으로 구분해야 합니다."),
-    INCORRECT_PRICE_RANGE(String.format("상품 가격은 %d원부터 시작해야 합니다.", VendingMachineConstants.LOWEST_PRICE));
+    INCORRECT_PRICE_RANGE(String.format("상품 가격은 %d원부터 시작해야 합니다.", VendingMachineConstants.LOWEST_PRICE)),
+    DUPLICATED_PRODUCT_NAME("상품명은 중복되지 않아야 합니다.");
 
     private final String message;
 
