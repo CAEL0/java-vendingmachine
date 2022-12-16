0cb14e1e9b066f5737bf629fb543d3e1415fe6cf
CAEL0 <kjongmin26@gmail.com>
2022-12-16 17:56:20 +0900

    fix(VendingMachineConstants): 정규표현식 수정

--- src/main/java/vendingmachine/constant/VendingMachineConstants.java
+++ src/main/java/vendingmachine/constant/VendingMachineConstants.java
@@ -4,7 +4,7 @@ public class VendingMachineConstants {
     public static final String PRODUCT_DELIMITER = ";";
     public static final String DELIMITER = ",";
 
-    public static final String PRODUCT_REGEX = "\\[\\w+,\\w+,\\w+\\]";
+    public static final String PRODUCT_REGEX = "\\[[\\w|가-힣]+,\\d+,\\d+\\]";
 
     public static final int LOWEST_PRICE = 100;
 }
