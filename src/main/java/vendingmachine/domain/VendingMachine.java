4a436e482b5e9256e56c201651fac85c36a5c1b1
CAEL0 <kjongmin26@gmail.com>
2022-12-16 13:48:21 +0900

    refactor(VendingMachine): productList 일급 클래스 사용

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -1,14 +1,10 @@
 package vendingmachine.domain;
 
-import vendingmachine.constant.ExceptionConstants;
-import vendingmachine.constant.VendingMachineConstants;
-
-import java.util.*;
-import java.util.regex.Pattern;
+import java.util.List;
 
 public class VendingMachine {
     private CoinQuantities coinQuantities;
-    private final List<Product> productList = new ArrayList<>();
+    private ProductList productList;
     private InsertedMoney insertedMoney;
 
     public void generateCoins(int holdingSum) {
@@ -16,37 +12,7 @@ public class VendingMachine {
     }
 
     public void addProducts(String productList) {
-        this.productList.clear();
-        Arrays.stream(productList.split(VendingMachineConstants.PRODUCT_DELIMITER))
-                .peek(this::validateProduct)
-                .forEach(this::addProduct);
-
-        validateDuplication();
-    }
-
-    private void validateProduct(String product) {
-        if (!Pattern.matches(VendingMachineConstants.PRODUCT_REGEX, product)) {
-            throw new IllegalArgumentException(ExceptionConstants.INCORRECT_PRODUCT_LIST.getMessage());
-        }
-    }
-
-    private void addProduct(String product) {
-        String parsedProduct = product.substring(1, product.length() - 1);
-        List<String> productInfo = List.of(parsedProduct.split(VendingMachineConstants.DELIMITER));
-
-        String name = productInfo.get(0);
-        String price = productInfo.get(1);
-        String amount = productInfo.get(2);
-
-        productList.add(new Product(name, price, amount));
-    }
-
-    private void validateDuplication() {
-        Set<String> productNames = new HashSet<>();
-        productList.forEach(product -> productNames.add(product.getName()));
-        if (productList.size() != productNames.size()) {
-            throw new IllegalArgumentException(ExceptionConstants.DUPLICATED_PRODUCT_NAME.getMessage());
-        }
+        this.productList = new ProductList(productList);
     }
 
     public void insertMoney(int insertedMoney) {
