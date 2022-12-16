eb7d6fc4a7ceb5f936233836dafecd4c5d0ddf43
CAEL0 <kjongmin26@gmail.com>
2022-12-16 14:12:30 +0900

    feat(ExceptionConstants): 구매 관련 에러 메시지 추가

--- src/main/java/vendingmachine/constant/ExceptionConstants.java
+++ src/main/java/vendingmachine/constant/ExceptionConstants.java
@@ -6,7 +6,9 @@ public enum ExceptionConstants {
     NOT_MULTIPLE_OF_TEN("입력은 10의 배수여야 합니다."),
     INCORRECT_PRODUCT_LIST("상품명, 가격, 수량은 쉼표로, 개별 상품은 대괄호로 묶어 세미콜론으로 구분해야 합니다."),
     INCORRECT_PRICE_RANGE(String.format("상품 가격은 %d원부터 시작해야 합니다.", VendingMachineConstants.LOWEST_PRICE)),
-    DUPLICATED_PRODUCT_NAME("상품명은 중복되지 않아야 합니다.");
+    DUPLICATED_PRODUCT_NAME("상품명은 중복되지 않아야 합니다."),
+    NO_SUCH_PRODUCT("자판기에 존재하지 않는 상품명입니다."),
+    NOT_ENOUGH_MONEY("구매하려는 상품의 금액이 남은 투입 금액보다 큽니다.");
 
     private final String message;
 
