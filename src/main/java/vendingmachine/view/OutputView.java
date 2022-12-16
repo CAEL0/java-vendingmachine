0d1ac77a61298fa317f60109afc5ce21dc329955
CAEL0 <kjongmin26@gmail.com>
2022-12-16 16:59:22 +0900

    refactor(OutputView): 인자 타입을 일급 클래스로 수정

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -1,13 +1,15 @@
 package vendingmachine.view;
 
 import vendingmachine.domain.Coin;
+import vendingmachine.domain.CoinQuantities;
+import vendingmachine.domain.InsertedMoney;
 
 import java.util.List;
 import java.util.stream.IntStream;
 
 public class OutputView {
 
-    public void printCoinQuantityList(List<Integer> coinQuantities) {
+    public void printCoinQuantityList(CoinQuantities coinQuantities) {
         System.out.println(ViewConstants.COIN_QUANTITY_LIST);
 
         List<Coin> coinList = List.of(Coin.values());
@@ -20,19 +22,19 @@ public class OutputView {
                 });
     }
 
-    public void printInsertedMoney(int insertedMoney) {
-        System.out.printf(ViewConstants.INSERTED_MONEY, insertedMoney);
+    public void printInsertedMoney(InsertedMoney insertedMoney) {
+        System.out.printf(ViewConstants.INSERTED_MONEY, insertedMoney.getInsertedMoney());
         System.out.println();
     }
 
-    public void printChange(List<Integer> coinQuantities, int insertedMoney) {
+    public void printChange(CoinQuantities coinQuantities, InsertedMoney insertedMoney) {
         System.out.println(ViewConstants.CHANGE);
 
         List<Coin> coinList = List.of(Coin.values());
         IntStream.range(0, 4)
                 .forEach(i -> {
                     int amount = coinList.get(i).getAmount();
-                    int quantity = Math.min(coinQuantities.get(i), insertedMoney / amount);
+                    int quantity = Math.min(coinQuantities.get(i), insertedMoney.getInsertedMoney() / amount);
                     System.out.printf(ViewConstants.COIN_QUANTITY, amount, quantity);
                     System.out.println();
                 });
