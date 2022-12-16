75635da497d1e4fcc635712a753dd49149c43349
CAEL0 <kjongmin26@gmail.com>
2022-12-16 13:46:36 +0900

    feat(ProductList): 상품 목록 일급 클래스 생성

---
+++ src/main/java/vendingmachine/domain/ProductList.java
@@ -0,0 +1,46 @@
+package vendingmachine.domain;
+
+import vendingmachine.constant.ExceptionConstants;
+import vendingmachine.constant.VendingMachineConstants;
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class ProductList {
+    private final List<Product> productList = new ArrayList<>();
+
+    public ProductList(String productList) {
+        List<String> splittedProductList = List.of(productList.split(VendingMachineConstants.PRODUCT_DELIMITER));
+        validateDuplication(splittedProductList.size());
+
+        Arrays.stream(productList.split(VendingMachineConstants.PRODUCT_DELIMITER))
+                .peek(this::validateProduct)
+                .forEach(this::addProduct);
+    }
+
+    private void validateDuplication(int size) {
+        Set<String> productNames = new HashSet<>();
+        productList.forEach(product -> productNames.add(product.getName()));
+
+        if (productNames.size() != size) {
+            throw new IllegalArgumentException(ExceptionConstants.DUPLICATED_PRODUCT_NAME.getMessage());
+        }
+    }
+
+    private void validateProduct(String product) {
+        if (!Pattern.matches(VendingMachineConstants.PRODUCT_REGEX, product)) {
+            throw new IllegalArgumentException(ExceptionConstants.INCORRECT_PRODUCT_LIST.getMessage());
+        }
+    }
+
+    private void addProduct(String product) {
+        String parsedProduct = product.substring(1, product.length() - 1);
+        List<String> productInfo = List.of(parsedProduct.split(VendingMachineConstants.DELIMITER));
+
+        String name = productInfo.get(0);
+        String price = productInfo.get(1);
+        String amount = productInfo.get(2);
+
+        productList.add(new Product(name, price, amount));
+    }
+}
